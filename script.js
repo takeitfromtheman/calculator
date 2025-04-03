a0cb0b4833ce193a8ba01355daa6a0f9233cdddd
takeitfromtheman <v.khristich@gmail.com>
2025-04-03 22:19:36 +0300

    Add variables to store input arguments and operator

--- script.js
+++ script.js
@@ -5,3 +5,8 @@ const subtract = (a, b) => a - b;
 const divide = (a, b) => (b === 0) ? 'Divided by zero' : a / b;
 
 const multiply = (a, b) =>  a * b;
+
+let arg1 = '';
+let arg2 = '';
+let operator = '';
+
